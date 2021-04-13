dd52a3cee060a08bf8f9a20235f7a6a33d6d30eb
Vivek Pandey <60037963+Vivek123-stack@users.noreply.github.com>
2021-04-13 02:27:16 -0700

    final
    Added payable function in assetcreate function and deleted cost variable from the contract

--- Asset.sol
+++ Asset.sol
@@ -1,11 +1,9 @@
 pragma solidity >=0.4.0 <0.9.0;
 
-//Creation of the contract
-contract AssetContract{
+    //Creation of the contract
+    contract AssetContract{
     //Declaration of the variables
     address owner;
-    /*address payable wallet;
-    uint256 price;*/
     uint public assetcount = 0;
     
     event Purchase(address indexed _buyer,uint _amount);
@@ -20,16 +18,13 @@ contract AssetContract{
     
     //Creation of the constructor
     constructor() public{
-        /*wallet =_wallet;
-        price = _price;*/
         owner = msg.sender;
     }
     
     //Creation of the mapping
-    mapping(address => bool)public Verified_owner;
-   // mapping(address => uint256)public Balances;
-    mapping(uint => assetcreation)public AssetCreation;
+    mapping(address => bool)public Verified_Owner;
     mapping(address => bool)public Verified_Transaction;
+    mapping(uint => assetcreation)public AssetCreation;
     
     //Creation of the array
     assettransfer[] public AssetTransfer;
@@ -38,7 +33,6 @@ contract AssetContract{
     struct assetcreation{
         
         uint _id;
-        uint _Cost;
         string _AssetName;
         string _CreaterName;
     }
@@ -50,35 +44,34 @@ contract AssetContract{
     }
     
     //Defining of the function
-    function assetcreate(uint _Cost, string memory _AssetName, string memory _CreaterName)public onlyOwner{
+    function assetcreate(string memory _AssetName, string memory _CreaterName)public payable onlyOwner{
         
         //Defining of the mapping
-        AssetCreation[assetcount]=assetcreation(assetcount,_Cost,_AssetName,_CreaterName);
+        AssetCreation[assetcount]=assetcreation(assetcount,_AssetName,_CreaterName);
         
         //Incrementing assetcount by 1
         assetcount += 1;
-        
-        //Record of the owner and transactions that the owner and transactions are correctly entered
-        Verified_owner[msg.sender]=true;
-        Verified_Transaction[msg.sender]=true;
     }
     
     function assettransfers(address _from,address _to)public{
-        
-        //Require statement for verifying that the owner and transaction cannot be done before
-        require(!Verified_owner[msg.sender]);
-        require(!Verified_Transaction[msg.sender]);
-        
+    
         //Defining of the array
         AssetTransfer.push(assettransfer(_from,_to));
     }
     
-    
-   /* function Buytoken()public payable{
+    function verifyowner()public returns(bool){
+        //Require statement for verifying that the owner and transaction cannot be done before
+        require(Verified_Owner[msg.sender]=true);
         
-        require(msg.value == price);
-        Balances[msg.sender] +=1;
-        wallet.transfer(msg.value);
-        emit Purchase(msg.sender,1);
-    }*/
+        //require previous owner cannot sell again his/her particular asset once its sold
+        //require(!Verified_Owner[msg.sender]);
+    }
+    
+    function verifytransactions()public returns(bool){
+        
+        //Require statement for verifying that the owner and transaction cannot be done before
+        require(Verified_Transaction[msg.sender]=true);
+    }
+    
+  
 }
