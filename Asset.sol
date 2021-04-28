121687e91f6925025972c5e9172f4a35a4d5201b
Vivek Pandey <60037963+Vivek123-stack@users.noreply.github.com>
2021-04-28 10:56:37 -0700

    Implemented ERC721 standard
    Implemented ERC721 standard, added mint function in assetcreate function, added burn function in contract, deleted verify owner and verify transactions functions from our contract.

--- Asset.sol
+++ Asset.sol
@@ -20,7 +20,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
     }
     
     //Creation of the constructor
-    constructor() public ERC721("",""){
+    constructor() public ERC721("nft original","NFT"){
         owner = msg.sender;
     }
     
@@ -40,27 +40,33 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
     }
     
     //Defining of the function
-    function assetCreate(string memory _AssetName, string memory _CreaterName,string memory _Discription_of_the_asset,string memory _Category_of_Asset)public onlyOwner{
+    function assetCreate(uint256 tokenId,string memory _AssetName, string memory _CreaterName,string memory _Discription_of_the_asset,string memory _Category_of_Asset)public onlyOwner{
         
         //Defining of the mapping
         AssetCreation[assetCount]=assetcreation(assetCount,_AssetName,_CreaterName,_Discription_of_the_asset,_Category_of_Asset);
         
         //Incrementing assetcount by 1
         assetCount += 1;
-    }
-    
-    function verifyOwner()public returns(bool){
+         
+        //Declaration of the variable and copying data from one datatype to another datatype
+        uint256 assetTokenClassId = assetCount;
+         
+        //Deffining the mint function from ERC721
+        _mint(msg.sender, assetTokenClassId);
+        
+        //Require statement to check that the token exits or not
+        require(_exists(tokenId));
+        
         //Require statement for verifying that the owner and transaction cannot be done before
         require(Verified_Owner[msg.sender]=true);
-        
-        //require previous owner cannot sell again his/her particular asset once its sold
-        //require(!Verified_Owner[msg.sender]);
-    }
-    
-    function verifyTransactions()public returns(bool){
         
         //Require statement for verifying that the owner and transaction cannot be done before
         require(Verified_Transaction[msg.sender]=true);
     }
     
+    function burnToken(uint256 tokenId) public {
+      
+      //Burn the token     
+     _burn(tokenId);
+    }
 }
